659269603fd5bea08205210003471baf673c5ee4
Konrad Gajewski <konrad.gajewski@physics.uu.se>
2024-01-19 17:25:56 +0100

    Updated the interlocks (names and logic). Not updated HMI panel. Updated opi.

--- cmds/st.cmd
+++ cmds/st.cmd
@@ -6,5 +6,6 @@ require recsync
 epicsEnvSet ("IOCNAME", "ioc01-ssa")
 
 iocshLoad("$(awakessa_DIR)ssa.iocsh", "TOP=/opt/epics/autosave,IP_ADDR=192.168.10.47,ASYN_PORT=SSA_PORT")
+#iocshLoad("$(awakessa_DIR)ssa.iocsh", "TOP=/opt/epics/autosave,IP_ADDR=192.168.1.234,ASYN_PORT=SSA_PORT")
 
 iocshLoad("$(recsync_DIR)/recsync.iocsh", "IOCNAME=$(IOCNAME)")
